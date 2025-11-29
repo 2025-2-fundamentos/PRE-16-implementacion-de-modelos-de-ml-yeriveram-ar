5d51ef7cc787fba5aeab2534642b056c60be259d
yeriveram-ar <yeriveram@unal.edu.co>
2025-11-28 20:23:54 -0500

    intento 1

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -6,5 +6,5 @@ import os
 def test_homework():
     """Test the homework"""
 
-    assert os.path.exists("homework/linear_regression.py")
-    assert os.path.exists("homework/example.py")
+    assert os.path.exists("../homework/linear_regression.py")
+    assert os.path.exists("../homework/example.py")
